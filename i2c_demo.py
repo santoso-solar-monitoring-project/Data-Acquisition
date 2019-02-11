5cb1fb5ec0c3f8b8b97387da9f3595121a00b03d
luisquint <luis.quint@utexas.edu>
2019-02-11 00:48:04 -0600

    Fixed i2c python demo

--- i2c_demo.py
+++ i2c_demo.py
@@ -1,14 +1,14 @@
-import smbus
+import smbus2
 import time
 # for RPI version 1, use “bus = smbus.SMBus(0)”
-bus = smbus.SMBus(1)
+bus = smbus2.SMBus(bus=1)
 
 # This is the address we setup in the Arduino Program
 address = 0x04
 
 
 def write_number(value):
-    bus.write_byte(address, value)
+    bus.write_byte(address, int(value))
     # bus.write_byte_data(address, 0, value)
     return -1
 
@@ -18,15 +18,15 @@ def read_number():
     # number = bus.read_byte_data(address, 1)
     return number
 
-    while True:
-        var = input("Enter 1 – 9: ")
-        if not var:
-            continue
+while True:
+    var = input("Enter 1 – 9: ")
+    if not var:
+        continue
 
     write_number(var)
-    print("RPI: Hi Arduino, I sent you ", var)
+    print("RPI: Hi Arduino, I sent you {}".format(var))
     # sleep one second
     time.sleep(1)
 
-    number = readNumber()
-    print("Arduino: Hey RPI, I received a digit ", number)
+    number = read_number()
+    print("Arduino: Hey RPI, I received a digit {}".format(number))
