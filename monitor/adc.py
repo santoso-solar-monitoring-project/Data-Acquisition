7cf8cac7b4050c2d9ca527083b9a5de095dcd283
luisquint <luis.quint@utexas.edu>
2019-02-24 16:47:11 -0600

    Commented out lines that caused bug

--- monitor/adc.py
+++ monitor/adc.py
@@ -1,11 +1,12 @@
-import numpy as np
+#import numpy as np
 
 
 class ADC:
 
     def __init__(self, debug=True):
         if debug:
-            self._samples = np.arange(1, 100)
+            self._samples = [1,2,3,4,5,6,7,8,9,10]
+#np.arange(1, 100)
             self._index = 0
         else:
             pass
@@ -15,5 +16,5 @@ class ADC:
         pass
         sample = self._samples[self._index]
         self._index += 1
-        self._index %= 100
+        self._index %= 10
         return sample
